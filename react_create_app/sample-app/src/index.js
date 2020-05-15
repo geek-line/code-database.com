f19aa4fc26482b40a1544132720edcdd419c55be
Rei Sugiura <reibomaru@g.ecc.u-tokyo.ac.jp>
2020-05-15 22:02:02 +0900

    create react_component

--- react_create_app/sample-app/src/index.js
+++ react_create_app/sample-app/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom';
-import TopPage    from './pages/Top.js'
+import TopPage    from './pages/TopPage.js'
 
 ReactDOM.render(
     React.createElement(TopPage),
